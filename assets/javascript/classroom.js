b692a6e727500c83297198e01185638d25396d57
annielngo3 <annielngo3@gmail.com>
2019-04-02 11:46:55 -0500

    added classroom page

--- assets/javascript/classroom.js
+++ assets/javascript/classroom.js
@@ -1,8 +1,1 @@
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
-
-
+$('#carouselFade').carousel();
